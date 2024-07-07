569e93b2affd7f2b74317cdbbab25344becda34b
Lucila Sol Aguilar <lucilasolaguilar@MacBook-Pro-de-Lucila.local>
2024-07-07 16:18:56 -0300

    se agregan los controller tal como estan en el diagrama de clases

---
+++ TPO/src/controller/LaboratoristaController.java
@@ -0,0 +1,11 @@
+package controller;
+
+public class LaboratoristaController {
+
+    public void cargarResultados(List<idPeticion> listaPeticion) { //TODO: No deberia ser una List<Integer>?
+    }
+
+    public void eliminarResultados() {  //TODO: Agregar id peticion como parametro.
+    }
+
+}
---
+++ TPO/src/controller/RecepcionController.java
@@ -0,0 +1,39 @@
+package controller;
+
+import model.Paciente;
+import model.Practica;
+import model.Sucursal;
+
+public class RecepcionController {
+
+    public void recibirPaciente() {
+    }
+
+    public void cargarPeticion(Paciente paciente, String obraSocial,
+                               Practica practica, Sucursal sucursal) {
+    }
+
+    public void darBajaPeticion() { //TODO: Agregar numero peticion como parametro.
+    }
+
+    public void modificarPeticion() { //TODO: Agregar numero peticion como parametro.
+    }
+
+    public void consultarResultado() { //TODO: Agregar id peticion como parametro.
+    }
+
+    public void darAltaPaciente() {
+    }
+
+    public void darBajaPaciente(int dniPaciente) {
+    }
+
+    public void modificarPaciente(int dniPaciente) {
+    }
+
+    public void solicitarResultados(int idPeticion) {
+    }
+
+    public void listarPeticionesCriticas() {
+    }
+}
